71c87b33f5bc1cf42a0f88f968ad403f7a4bf9d1
rublev <r@rublev.io>
2025-10-17 22:04:29 -0400

    fix: dark mode/light mode styles in <app>

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+useHead({
+  bodyAttrs: {
+    class: 'antialiased min-h-screen bg-background text-foreground font-sans',
+  },
+})
+</script>
+
 <template>
   <NuxtRouteAnnouncer />
   <NuxtLayout>
