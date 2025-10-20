4594d9e20048b36db7ef7d958eb438d938a80939
rublev <r@rublev.io>
2025-10-19 21:04:38 -0400

    feat: add 'selection-' highlight classes to tailwind ignore

--- eslint.config.ts
+++ eslint.config.ts
@@ -37,7 +37,12 @@ export default antfu(
       'better-tailwindcss/no-unregistered-classes': [
         'error',
         {
-          ignore: ['^pixel-rounded', '^xerox$', '^svg-filter--hidden$'],
+          ignore: [
+            '^pixel-rounded',
+            '^xerox$',
+            '^svg-filter--hidden$',
+            '^selection',
+          ],
         },
       ],
       /**
