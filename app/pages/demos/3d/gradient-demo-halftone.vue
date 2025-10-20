827d989f77b5ed31300a860cd422ab3a122e4dd6
rublev <r@rublev.io>
2025-10-19 21:09:13 -0400

    fix: linting updates

--- app/pages/demos/3d/gradient-demo-halftone.vue
+++ app/pages/demos/3d/gradient-demo-halftone.vue
@@ -94,10 +94,10 @@ function hexToRgb(hex: string) {
   const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
   return result
     ? new THREE.Vector3(
-      Number.parseInt(result[1]!, 16),
-      Number.parseInt(result[2]!, 16),
-      Number.parseInt(result[3]!, 16),
-    )
+        Number.parseInt(result[1]!, 16),
+        Number.parseInt(result[2]!, 16),
+        Number.parseInt(result[3]!, 16),
+      )
     : new THREE.Vector3(255, 255, 255)
 }
 
